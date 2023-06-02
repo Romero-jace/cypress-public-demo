41bdf8d4d3e0ce8b942faf2ca53ad27bd4d9a1ea
Jace Romero <39967529+Romero-jace@users.noreply.github.com>
2023-06-02 11:08:15 -0500

    lint fixes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,8 +24,8 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add("requestAndVerify200", (page) => {
-  cy.request(page.prop("href")).then((response) => {
-    expect(response.status).to.eq(200);
-  });
-});
+Cypress.Commands.add('requestAndVerify200', (page) => {
+  cy.request(page.prop('href')).then((response) => {
+    expect(response.status).to.eq(200)
+  })
+})
